7d824db9fe981b6d91cbd07006c87494178577fe
Kanybek <kanybektlgn0@gmail.com>
2024-01-10 15:44:00 +0600

    feat: configure scss loader in webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,71 @@
 import path from "path";
 import { Configuration } from "webpack";
-import HTMLWebpackPlugin from 'html-webpack-plugin'
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const isDevelopment = process.env.NODE_ENV === "development";
 
 const config: Configuration = {
-    mode:
-      (process.env.NODE_ENV as "production" | "development" | undefined) ??
-      "development",
-    entry: "./src/index.tsx",
-    module: {
-      rules: [
-        {
-          test: /.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-      new HTMLWebpackPlugin({
-       template: 'public/index.html'
-      }),
+  mode: process.env.NODE_ENV as "production" | "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
-  };
-  
-  export default config;
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
+};
+
+export default config;
