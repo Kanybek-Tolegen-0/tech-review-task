491c29bed7185f9caeec41fc592cc2d3ac6c7cbe
Kanybek <kanybektlgn0@gmail.com>
2024-01-10 19:17:30 +0600

    refactor: reconfigure eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
 const rootElement = document.getElementById('root')
 
-if(rootElement) {
-    const root = createRoot(rootElement)
+if (rootElement) {
+	const root = createRoot(rootElement)
 
-    root.render(
-        <StrictMode>
-            <App />
-        </StrictMode>
-    )
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>
+	)
 }
